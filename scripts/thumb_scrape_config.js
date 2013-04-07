4a32219666e7bbd56f1274f57ef093e0b90d7346
Gabriel Dunne <gdunne@quilime.com>
2013-04-06 17:48:10 -0700

    Updated Scraper.

--- scripts/thumb_scrape_config.js
+++ scripts/thumb_scrape_config.js
@@ -1,5 +1,6 @@
 var urls = [
 	'http://archive.org/movies/thumbnails.php?identifier=0924_Greatest_Name_in_Wine_The_11_00_13_15'
+, 'http://archive.org/movies/thumbnails.php?identifier=Tuna_Seining_and_Porpoise_Safety_04_22_43_00'
 ];
 
 pjs.config({
@@ -14,17 +15,17 @@ pjs.config({
 pjs.addSuite({
   url: urls,
   scrapers: [
-  function() {
-		var r = {};
-		var thumbnail_links = $('.box div a').map(function() {
-        return {
-					img: $('img', this).attr('src'),
-					link: $(this).attr('href')
-        }
-        }).toArray();
-		r['url'] = window.location.href;
-		r['thumbnails'] = thumbnail_links;
-		return r;
-  }
-]
+	  function() {
+			var r = {};
+			var thumbnail_links = $('.box div a').map(function() {
+	        return {
+						img: $('img', this).attr('src'),
+						link: $(this).attr('href')
+	        }
+	        }).toArray();
+			r['id'] = window.location.href.split('=')[1];
+			r['thumbnails'] = thumbnail_links;
+			return r;
+	  }
+	]
 });
