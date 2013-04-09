6d4b0abcacacd406a769d838face8a6439eed2a9
Gabriel Dunne <gdunne@quilime.com>
2013-04-08 23:47:57 -0700

    Added new data.

--- scripts/compile_json.js
+++ scripts/compile_json.js
@@ -1,31 +1,37 @@
-var ids = require('./prelinger_identifiers.json'); ids = ids.identifiers;
-var extended = require('./prelinger_extended.json'); extended_clips = extended.clips;
-var thumbs = require('./prelinger_thumbs.json');
+var ids = require('../data/prelinger_identifiers.json'); ids = ids.identifiers;
+var extended = require('../data/prelinger_extended.json'); extended_clips = extended.clips;
+var anim_thumbs = require('../data/prelinger_animgifs.json');
+var static_thumbs = require('../data/prelinger_static_thumbs.json');
 
 var output = { "clips" : [] };
+for( var i = 0; i < ids.length; i++) {
+  var id = ids[i];
 
-for (var i = 0; i < extended_clips.length; i++) {
-  var id = extended_clips[i].id;
-  for (var j = 0; j < thumbs.length; j++) {
-    var n = thumbs[j].match("/" + id + "/");
-    if (n && n.length > 0) {
-      var thumbnail = thumbs[j].split('?')[0];
-      var thumbnail_filename = thumbnail.split('/');
-      thumbnail_filename = thumbnail_filename[thumbnail_filename.length-1];
-      output.clips.push({
-        "date" : extended_clips[i].date,
-        "description" : extended_clips[i].description,
-        "subject" : extended_clips[i].subject,
-        "id" : id,
-        "title": extended_clips[i].title,
-        "url" : "http://archive.org/details/" + id,
-        "thumbnails_url" : "http://archive.org/movies/thumbnails.php?identifier=" + id,
-        "thumbnail" : thumbnail,
-        "thumbnail_filename" : thumbnail_filename
-        }
-      );
-    }
+  // process animated gifs
+  var n = anim_thumbs[i].match("/" + id + "/");
+  var anim_thumb;
+  if ( n && n.length > 0) {
+    anim_thumb = anim_thumbs[i].split('?')[0];
   }
+
+  // process static thumbs
+  var static_thumb = [];
+  if (static_thumbs[i]) {
+    static_thumb = static_thumbs[i]['thumbnails'];
+  }
+
+  // create output
+  output.clips.push({
+    "id" : id,
+    "date" : extended_clips[i].date,
+    "description" : extended_clips[i].description,
+    "subject" : extended_clips[i].subject,
+    "title": extended_clips[i].title,
+    "url" : "http://archive.org/details/" + id,
+    "thumbnail_anim" : anim_thumb,
+    "thumbnails_url" : "http://archive.org/movies/thumbnails.php?identifier=" + id,
+    "thumbnails" : static_thumb
+  });
 }
 
 console.log(JSON.stringify(output));
